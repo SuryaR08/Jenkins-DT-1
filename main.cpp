16f41d30b87363fca7103950bd663d5b33f58c64
SuryaR08 <suryaraja8903@gmail.com>
2025-02-08 10:43:20 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "This message printed via triggers on every push to github repo!!!......" << std::endl;
+    std::cout << "This message printed via triggers on every push to github repo!!!....." << std::endl;
     return 0;
 }
